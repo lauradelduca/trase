a9cbed6857c93a6b171857057188c976858f4644
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2019-04-08 10:34:54 +0100

    Remove the logic with ranking chart - flow queries are too slow

--- app/services/api/v3/dashboards/parametrised_charts.rb
+++ app/services/api/v3/dashboards/parametrised_charts.rb
@@ -72,15 +72,7 @@ module Api
         def single_year_no_ncont_node_type_view(node_type)
           {
             source: :single_year_no_ncont_node_type_view,
-            type: SingleYearCharts::ChartType.call(
-              data: SingleYearCharts::PrepareData.call(
-                chart_params: @chart_params,
-                top_n: TOP_N,
-                node_type_idx: Api::V3::NodeType.node_index_for_id(@context, node_type.id),
-                type: :no_ncont
-              ),
-              default_chart_type: HORIZONTAL_BAR_CHART
-            ),
+            type: HORIZONTAL_BAR_CHART,
             x: :node_type,
             node_type_id: node_type.id
           }
